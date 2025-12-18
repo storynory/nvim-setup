8bc894254b24726fb9a7055b3b842d5fd0de5d89
hugh Fraser <hugh@storynory.com>
2025-12-18 12:13:31 +0000

    treesitter change

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,31 +1,37 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	event = { "BufReadPost", "BufNewFile" },
-	opts = {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufNewFile" },
 
+		opts = {
+			ensure_installed = {
+				"lua",
+				"html",
+				"css",
+				"javascript",
+				"svelte",
+				"json",
+				"markdown",
+				"markdown_inline",
+			},
 
-		ensure_installed = {
-			"lua",
-			"html",
-			"css",
-			"javascript",
-			"svelte",
-			"lua",
-			"json",
-			"markdown",
-			"markdown_inline",
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+
+			indent = {
+				enable = true,
+			},
 		},
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = false,
-		},
-		indent = {
-			enable = true,
-		},
+
+		config = function(_, opts)
+			local ok, configs = pcall(require, "nvim-treesitter.configs")
+			if not ok then
+				return
+			end
+			configs.setup(opts)
+		end,
 	},
-
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
-	end,
 }
